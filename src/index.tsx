5735ae294db6005513aaeeb26b602db1aee5d758
s-nakai <s-nakai@nisso.co.jp>
2020-10-21 16:39:02 +0900

    modify

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,10 @@ import * as serviceWorker from "./serviceWorker";
 
 import SWUpdateDialog from "./SWUpdateDialog";
 
-if (process.env.NODE_ENV !== "production") {
-  const { whyDidYouUpdate } = require("why-did-you-update");
-  whyDidYouUpdate(React);
-}
+// if (process.env.NODE_ENV !== "production") {
+//   const { whyDidYouUpdate } = require("why-did-you-update");
+//   whyDidYouUpdate(React);
+// }
 
 ReactDOM.render(
   // <React.StrictMode>
